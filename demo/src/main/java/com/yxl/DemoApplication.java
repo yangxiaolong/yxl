b95d3060972a00bfd044f158d4ff374a23e77ee2
yangxiaolong <626571494@qq.com>
2024-09-13 13:51:59 +0800

    delete db dependence

--- demo/src/main/java/com/yxl/DemoApplication.java
+++ demo/src/main/java/com/yxl/DemoApplication.java
@@ -2,9 +2,10 @@ package com.yxl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class DemoApplication {
 
