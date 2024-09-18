c5821ac79e84dc47abf1da86381b7cce1a38db90
yangxiaolong <626571494@qq.com>
2024-09-18 16:14:43 +0800

    lazyload optimise

--- loader/loader-demo/src/main/java/com/lego/yxl/LoaderApplication.java
+++ loader/loader-demo/src/main/java/com/lego/yxl/LoaderApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * https://mp.weixin.qq.com/s/5q6DohyVSS4JyprUibt1Sg
+ */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class LoaderApplication {
