2f9383b8412d8c0e7bdad0d4c20753eec5b33dbc
yangxiaolong <626571494@qq.com>
2024-09-18 14:46:00 +0800

    optimize

--- loader/loader-core/src/main/java/com/lego/yxl/core/support/LazyLoaderInterceptorFactory.java
+++ loader/loader-core/src/main/java/com/lego/yxl/core/support/LazyLoaderInterceptorFactory.java
@@ -11,32 +11,30 @@ import java.util.function.Function;
 
 import static java.util.stream.Collectors.toMap;
 
-/**
-
- */
 public class LazyLoaderInterceptorFactory {
-    private final Map<Class, Map<String, PropertyLazyLoader>> loaderCache = Maps.newHashMap();
+
+    private final Map<Class<?>, Map<String, PropertyLazyLoader>> loaderCache = Maps.newHashMap();
     private final PropertyLazyLoaderFactory propertyLazyLoaderFactory;
 
     public LazyLoaderInterceptorFactory(PropertyLazyLoaderFactory propertyLazyLoaderFactory) {
         this.propertyLazyLoaderFactory = propertyLazyLoaderFactory;
     }
 
-
-    public LazyLoaderInterceptor createFor(Class cls, Object target){
-        Map<String, PropertyLazyLoader>  loaders = this.loaderCache.computeIfAbsent(cls,
-                targetCls -> createForClass(targetCls));
+    public LazyLoaderInterceptor createFor(Class<?> cls, Object target) {
+        Map<String, PropertyLazyLoader> loaders = this.loaderCache.computeIfAbsent(cls,
+                this::createForClass);
 
         return new LazyLoaderInterceptor(loaders, target);
     }
 
-    private Map<String, PropertyLazyLoader> createForClass(Class targetCls) {
+    private Map<String, PropertyLazyLoader> createForClass(Class<?> targetCls) {
         List<PropertyLazyLoader> propertyLazyLoaders = propertyLazyLoaderFactory.createFor(targetCls);
-        if (CollectionUtils.isEmpty(propertyLazyLoaders)){
+        if (CollectionUtils.isEmpty(propertyLazyLoaders)) {
             return Collections.emptyMap();
         }
         return propertyLazyLoaders.stream()
                 .filter(Objects::nonNull)
                 .collect(toMap(loader -> loader.getField().getName(), Function.identity()));
     }
+
 }
