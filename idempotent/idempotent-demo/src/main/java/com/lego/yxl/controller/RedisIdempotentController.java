ca3074a2fcd355e6c5c861a1e9be247c0035131c
yangxiaolong <626571494@qq.com>
2024-09-26 22:04:33 +0800

    idempotent

--- idempotent/idempotent-demo/src/main/java/com/lego/yxl/controller/RedisIdempotentController.java
+++ idempotent/idempotent-demo/src/main/java/com/lego/yxl/controller/RedisIdempotentController.java
@@ -1,19 +1,19 @@
 package com.lego.yxl.controller;
 
 import com.lego.yxl.service.BaseIdempotentService;
-import com.lego.yxl.service.DBBasedIdempotentService;
+import com.lego.yxl.service.RedisBasedIdempotentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/db/idempotent")
+@RequestMapping(value = "/redis/idempotent")
 @Slf4j
-public class DbIdempotentController extends IdempotentController {
+public class RedisIdempotentController extends IdempotentController {
 
     @Autowired
-    DBBasedIdempotentService idempotentService;
+    private RedisBasedIdempotentService idempotentService;
 
     @Override
     BaseIdempotentService getIdempotentService() {
