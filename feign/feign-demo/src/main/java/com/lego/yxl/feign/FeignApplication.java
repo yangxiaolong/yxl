83651aded7266a6e6158ea76161d55f18ad79834
yangxiaolong <626571494@qq.com>
2024-09-29 22:04:29 +0800

    feign exception manage

--- feign/feign-demo/src/main/java/com/lego/yxl/feign/FeignApplication.java
+++ feign/feign-demo/src/main/java/com/lego/yxl/feign/FeignApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * https://mp.weixin.qq.com/s/sounmvvkCWaAaT2EYvohVA
+ */
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class FeignApplication {
