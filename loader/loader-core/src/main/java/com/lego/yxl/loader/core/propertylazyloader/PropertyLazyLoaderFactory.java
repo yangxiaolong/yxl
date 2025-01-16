40f1d44d97380fecea15a2cac42532becd877099
Dragon <626571494@qq.com>
2025-01-16 22:55:12 +0800

    lazyloader update

--- loader/loader-core/src/main/java/com/lego/yxl/loader/core/propertylazyloader/PropertyLazyLoaderFactory.java
+++ loader/loader-core/src/main/java/com/lego/yxl/loader/core/propertylazyloader/PropertyLazyLoaderFactory.java
@@ -12,7 +12,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,24 +26,17 @@ public class PropertyLazyLoaderFactory {
     public List<PropertyLazyLoader> createFor(Class<?> cls) {
         return FieldUtils.getAllFieldsList(cls).stream()
                 .map(this::createFromField)
-                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
     private PropertyLazyLoader createFromField(Field field) {
         LazyLoadBy mergedAnnotation = AnnotatedElementUtils.findMergedAnnotation(field, LazyLoadBy.class);
-        if (mergedAnnotation == null) {
-            return null;
-        }
 
         String targetEl = mergedAnnotation.value();
         Annotation[] annotations = field.getAnnotations();
         for (Annotation annotation : annotations) {
             AnnotationAttributes mergedAnnotationAttributes = AnnotatedElementUtils.findMergedAnnotationAttributes(
                     field, annotation.annotationType(), true, true);
-            if (Objects.isNull(mergedAnnotationAttributes)) {
-                continue;
-            }
             Set<Map.Entry<String, Object>> entries = mergedAnnotationAttributes.entrySet();
             if (CollectionUtils.isEmpty(entries)) {
                 continue;
