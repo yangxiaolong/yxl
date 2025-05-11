16660e437cdb9125d20080b57e1b3e788f4c35d7
Dragon <626571494@qq.com>
2025-05-11 21:42:02 +0800

    delay

--- delay/delay-starter/src/main/java/com/lego/yxl/delay/starter/DelayBasedRocketMQAutoConfiguration.java
+++ delay/delay-starter/src/main/java/com/lego/yxl/delay/starter/DelayBasedRocketMQAutoConfiguration.java
@@ -10,15 +10,18 @@ import org.springframework.aop.PointcutAdvisor;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
 import org.springframework.core.env.Environment;
 
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureAfter(RocketMQAutoConfiguration.class)
 @ConditionalOnBean(RocketMQTemplate.class)
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class DelayBasedRocketMQAutoConfiguration {
 
     @Autowired
@@ -28,16 +31,19 @@ public class DelayBasedRocketMQAutoConfiguration {
     private RocketMQTemplate rocketMQTemplate;
 
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public DelayMethodInterceptor delayInterceptor() {
         return new DelayMethodInterceptor(this.environment, this.rocketMQTemplate);
     }
 
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public DelayConsumerContainerRegistry delayConsumerContainerRegistry() {
         return new DelayConsumerContainerRegistry(this.environment);
     }
 
     @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public PointcutAdvisor delayPointcutAdvisor(DelayMethodInterceptor delayMethodInterceptor) {
         Pointcut pointcut = new AnnotationMatchingPointcut(null, DelayBasedRocketMQ.class);
         return new DefaultPointcutAdvisor(pointcut, delayMethodInterceptor);
